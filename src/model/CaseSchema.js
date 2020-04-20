46c237c2e32c0a959a8199d8c96cd7abf9b9e96e
kims3939 <kims3939@gmail.com>
2020-04-20 09:07:33 +0900

    mongodb added

---
+++ src/model/CaseSchema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = require('./UserSchema');
+const CommentSchema = require('./CommentSchema');
+
+const CaseSchema = Schema({
+
+});
+
+module.exports = CaseSchema;
---
+++ src/model/UserSchema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = Schema({
+    username:String,
+    email:String,
+    password:String,
+    speciality:[String]
+});
+
+module.exports = UserSchema;
